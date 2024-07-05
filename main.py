004262aceb85c3282916de7f3000b9a1f7ea0c9b
Alex Kiselyov <akiselyov@ukr.net>
2024-07-06 03:27:15 +0400

    Refactoring

--- main.py
+++ main.py
@@ -8,137 +8,75 @@ from lol.generator import generate_lol_items
 from rgm.generator import generate_swarm_augments
 from tft.generator import generate_tft_units, generate_tft_traits, generate_tft_items, generate_tft_augments
 
+def rm_temp_cache(version=''):
+    #return
+    temp_cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_temp', version)
+
+    if os.path.exists(temp_cache_dir):
+        shutil.rmtree(temp_cache_dir)
+
 def main():
     arg_parser = argparse.ArgumentParser()
     subparsers = arg_parser.add_subparsers(dest="cmd")
 
-    parser_items = subparsers.add_parser("lol-items", help="Generates League of Legends items.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-    parser_items.add_argument("--icons", action=argparse.BooleanOptionalAction, default=False, help="Generate item icons.")
+    cmd_configs = {
+        "lol-items": {"help": "Generates League of Legends items.", "func": generate_lol_items, "extra_args": {"--icons": {"action": argparse.BooleanOptionalAction, "default": False, "help": "Generate item icons."}}},
+        "swarm-augments": {"help": "Generates LoL Swarm augments.", "func": generate_swarm_augments},
+        "tft": {"help": "Generates Teamfight Tactics units, traits, items, and augments.", "func": generate_tft_all},
+        "tft-units": {"help": "Generates Teamfight Tactics units.", "func": generate_tft_units},
+        "tft-traits": {"help": "Generates Teamfight Tactics traits.", "func": generate_tft_traits},
+        "tft-items": {"help": "Generates Teamfight Tactics items.", "func": generate_tft_items},
+        "tft-augments": {"help": "Generates Teamfight Tactics augments.", "func": generate_tft_augments},
+        "staticons": {"help": "Generates stat icons used in tooltips for abilities, items, etc.", "func": process_staticons}
+    }
 
-    parser_items = subparsers.add_parser("swarm-augments", help="Generates LoL Swarm augments.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_items = subparsers.add_parser("tft", help="Generates Teamfight Tactics units, traits, items, and augments.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_items = subparsers.add_parser("tft-units", help="Generates Teamfight Tactics units.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_items = subparsers.add_parser("tft-traits", help="Generates Teamfight Tactics traits.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_items = subparsers.add_parser("tft-items", help="Generates Teamfight Tactics items.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_items = subparsers.add_parser("tft-augments", help="Generates Teamfight Tactics augments.")
-    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
-    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
-    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
-
-    parser_staticons = subparsers.add_parser("staticons", help="Generates stat icons used in tooltips for abilities, items, etc.")
-    parser_staticons.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
-    parser_staticons.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
+    for cmd, config in cmd_configs.items():
+        parser = subparsers.add_parser(cmd, help=config["help"])
+        add_common_args(parser)
+        if "extra_args" in config:
+            for arg, kwargs in config["extra_args"].items():
+                parser.add_argument(arg, **kwargs)
 
     args = arg_parser.parse_args()
 
-    if args.cmd == "lol-items":
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
-        
-        if args.version[0] == 'all':
-            args.version = cd_get_versions_clean()
+    if args.cmd in cmd_configs:
+        extra_args = []
+        if args.cmd == "lol-items":
+            extra_args.append(args.icons)
 
-        for version in args.version:
-            generate_lol_items(version, output_dir, args.lang, args.cache, args.icons)
-
-            if re.match(r'^\d+\.\d+$', version) or version in ['latest', 'pbe']:
-                rm_temp_cache(version)
-
-    elif args.cmd == "swarm-augments":
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
-        
-        if args.version[0] == 'all':
-            args.version = cd_get_versions_clean()
-
-        for version in args.version:
-            generate_swarm_augments(version, output_dir, args.lang, args.cache)
-
-            if re.match(r'^\d+\.\d+$', version) or version in ['latest', 'pbe']:
-                rm_temp_cache(version)
-
-    elif args.cmd == "tft":
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
-
-        if args.version[0] == 'all':
-            args.version = cd_get_versions_clean()
-        
-        for version in args.version:
-            generate_tft_units(version, output_dir, args.lang, args.cache)
-            generate_tft_traits(version, output_dir, args.lang, args.cache)
-            generate_tft_items(version, output_dir, args.lang, args.cache)
-            generate_tft_augments(version, output_dir, args.lang, args.cache)
-
-            if re.match(r'^\d+\.\d+$', version) or version in ['latest', 'pbe']:
-                rm_temp_cache(version)
-
-    elif args.cmd in ["tft-units", "tft-traits", "tft-items", "tft-augments"]:
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
-
-        if args.version[0] == 'all':
-            args.version = cd_get_versions_clean()
-
-        for version in args.version:
-            match args.cmd:
-                case "tft-units":
-                    generate_tft_units(version, output_dir, args.lang, args.cache)
-                case "tft-traits":
-                    generate_tft_traits(version, output_dir, args.lang, args.cache)
-                case "tft-items":
-                    generate_tft_items(version, output_dir, args.lang, args.cache)
-                case "tft-augments":
-                    generate_tft_augments(version, output_dir, args.lang, args.cache)
-
-            if re.match(r'^\d+\.\d+$', version) or version in ['latest', 'pbe']:
-                rm_temp_cache(version)
-
-    elif args.cmd == "staticons":
-        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
-
-        if args.version[0] == 'all':
-            args.version = cd_get_versions_clean()
-
-        for version in args.version:
-            AtlasProcessor().process_staticons(version, output_dir)
-
+        process_versions(args, cmd_configs[args.cmd]["func"], *extra_args)
     else:
         arg_parser.print_help()
 
     rm_temp_cache()
 
-def rm_temp_cache(version=''):
-    temp_cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_temp', version)
+def add_common_args(parser_items):
+    parser_items.add_argument("-v", "--version", nargs='+', metavar="VERSION", default=["pbe"], help="Version of the game (e.g., 11.1, 13.23, latest, pbe, all).")
+    parser_items.add_argument("-l", "--lang", nargs='+', metavar="LANGUAGE", default=["all"], help="Language of the game (e.g., en_us, ru_ru, zh_cn, all).")
+    parser_items.add_argument("-o", "--output", metavar="PATH", default="export", help="Output path.")
+    parser_items.add_argument("--cache", action=argparse.BooleanOptionalAction, default=False, help="Use Redis cache.")
 
-    if os.path.exists(temp_cache_dir):
-        shutil.rmtree(temp_cache_dir)
+def handle_versions(args):
+    if args.version[0] == 'all':
+        args.version = cd_get_versions_clean()
+    return args.version
+
+def process_versions(args, generate_func, *extra_args):
+    output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.output)
+    versions = handle_versions(args)
+    for version in versions:
+        generate_func(version, output_dir, args.lang, args.cache, *extra_args)
+        if re.match(r'^\d+\.\d+$', version) or version in ['latest', 'pbe']:
+            rm_temp_cache(version)
+
+def generate_tft_all(version, output_dir, lang, cache):
+    generate_tft_units(version, output_dir, lang, cache)
+    generate_tft_traits(version, output_dir, lang, cache)
+    generate_tft_items(version, output_dir, lang, cache)
+    generate_tft_augments(version, output_dir, lang, cache)
+
+def process_staticons(version, output_dir, lang, cache):
+    AtlasProcessor().process_staticons(version, output_dir)
 
 if __name__ == "__main__":
     main()
