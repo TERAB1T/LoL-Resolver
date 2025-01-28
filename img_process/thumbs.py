c0480b1177bfbab93f71559af4700ae99f6746b0
Alex Kiselyov <akiselyov@ukr.net>
2025-01-28 13:21:15 +0400

    Thumb Generator: added tft-arenas, tft-booms, tft-tactics

--- img_process/thumbs.py
+++ img_process/thumbs.py
@@ -4,14 +4,14 @@ from utils import *
 import aiohttp
 import asyncio
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageOps
+from tqdm.asyncio import tqdm
 
 class ThumbGenerator:
     def __init__(self, version, output_dir, cache):
         self.version = version
         self.output_dir = os.path.join(output_dir, f"thumbs") # TEMP!!!
         self.cache = cache
-        self.semaphore = asyncio.Semaphore(10)
 
         self.last_modified = None
         self.redis_con = None
@@ -44,6 +44,9 @@ class ThumbGenerator:
 
         if not self.is_cached:
             self._process_lol_skins()
+            self._process_tft_arenas()
+            self._process_tft_booms()
+            self._process_tft_tactics()
 
         if cache:
             if self.redis_con:
@@ -110,48 +113,23 @@ class ThumbGenerator:
             elif "error" in patch_status:
                 self.is_cached = True
                 print(f"Version {self.version}: The latest CDragon update failed. Please try again later.")
-
-    def _process_lol_skins(self):
-        alias = "lol-skins"
-        url = f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/v1/skins.json"
-        response = urllib3.request("GET", url)
-
-        if response.status == 200:
-            output_dir = os.path.join(self.output_dir, alias)
-            os.makedirs(output_dir, exist_ok=True)
-
-            skins_raw = ujson.loads(response.data)
-            skin_list = set()
-
-            for skin in skins_raw.values():
-                img_path = skin.get("tilePath", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
-                skin_list.add(img_path)
-
-                alt_versions = skin.get("questSkinInfo")
-
-                if alt_versions:
-                    alt_versions_tiers = alt_versions.get("tiers", [])
-
-                    for alt_version in alt_versions_tiers:
-                        img_path = alt_version.get("tilePath", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
-                        skin_list.add(img_path)
-
-            asyncio.run(self._process_urls(skin_list, [[50, 50], [100, 100]], output_dir, alias))
     
     async def _process_urls(self, urls, sizes, output_dir, alias):
-        tasks = [self._process_image(url, sizes, output_dir, alias) for url in urls]
-        await asyncio.gather(*tasks)
+        semaphore = asyncio.Semaphore(20)
+        with tqdm(total=len(urls), desc=f"Generating thumbnails ({alias})", unit="image", bar_format="{desc}: {n_fmt}/{total_fmt} |{bar:20}|") as pbar:
+            tasks = [self._process_image(url, sizes, output_dir, alias, semaphore, pbar) for url in urls]
+            await asyncio.gather(*tasks)
 
-    async def _process_image(self, url, sizes, output_dir, alias):
+    async def _process_image(self, url, sizes, output_dir, alias, semaphore, pbar):
         img_without_extension = os.path.splitext(os.path.basename(url))[0]
-        async with self.semaphore:
+        async with semaphore:
             async def inner_process_image(session):
                 async with session.get(url) as response:
                     if response.status == 200:
                         for size in sizes:
                             img = await response.read()
                             img = Image.open(BytesIO(img))
-                            img.thumbnail(size)
+                            img = ImageOps.fit(img, size, Image.LANCZOS)
                             img.save(f"{output_dir}/{img_without_extension}_{size[0]}x{size[1]}.webp", "WEBP")
                             img.close()
 
@@ -171,3 +149,87 @@ class ThumbGenerator:
                                 
                 else:        
                     await inner_process_image(session)
+        pbar.update(1)
+
+    ### TYPES ###
+
+    def _process_lol_skins(self):
+        alias = "lol-skins"
+        url = f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/v1/skins.json"
+        response = urllib3.request("GET", url)
+
+        if response.status == 200:
+            output_dir = os.path.join(self.output_dir, alias)
+            os.makedirs(output_dir, exist_ok=True)
+
+            raw_data = ujson.loads(response.data)
+            output_list = set()
+
+            for entry in raw_data.values():
+                img_path = entry.get("tilePath", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
+                output_list.add(img_path)
+
+                alt_versions = entry.get("questSkinInfo")
+
+                if alt_versions:
+                    alt_versions_tiers = alt_versions.get("tiers", [])
+
+                    for alt_version in alt_versions_tiers:
+                        img_path = alt_version.get("tilePath", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
+                        output_list.add(img_path)
+
+            asyncio.run(self._process_urls(output_list, [[50, 50], [100, 100]], output_dir, alias))
+
+    def _process_tft_arenas(self):
+        alias = "tft-arenas"
+        url = f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/v1/tftmapskins.json"
+        response = urllib3.request("GET", url)
+
+        if response.status == 200:
+            output_dir = os.path.join(self.output_dir, alias)
+            os.makedirs(output_dir, exist_ok=True)
+
+            raw_data = ujson.loads(response.data)
+            output_list = set()
+
+            for entry in raw_data:
+                img_path = entry.get("loadoutsIcon", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
+                output_list.add(img_path)
+
+            asyncio.run(self._process_urls(output_list, [[100, 100]], output_dir, alias))
+
+    def _process_tft_booms(self):
+        alias = "tft-booms"
+        url = f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/v1/tftdamageskins.json"
+        response = urllib3.request("GET", url)
+
+        if response.status == 200:
+            output_dir = os.path.join(self.output_dir, alias)
+            os.makedirs(output_dir, exist_ok=True)
+
+            raw_data = ujson.loads(response.data)
+            output_list = set()
+
+            for entry in raw_data:
+                img_path = entry.get("loadoutsIcon", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
+                output_list.add(img_path)
+
+            asyncio.run(self._process_urls(output_list, [[100, 100]], output_dir, alias))
+
+    def _process_tft_tactics(self):
+        alias = "tft-tactics"
+        url = f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/v1/companions.json"
+        response = urllib3.request("GET", url)
+
+        if response.status == 200:
+            output_dir = os.path.join(self.output_dir, alias)
+            os.makedirs(output_dir, exist_ok=True)
+
+            raw_data = ujson.loads(response.data)
+            output_list = set()
+
+            for entry in raw_data:
+                img_path = entry.get("loadoutsIcon", "").lower().replace("/lol-game-data/assets/assets/", f"https://raw.communitydragon.org/{self.version}/plugins/rcp-be-lol-game-data/global/default/assets/")
+                output_list.add(img_path)
+
+            asyncio.run(self._process_urls(output_list, [[100, 100]], output_dir, alias))
