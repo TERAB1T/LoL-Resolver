d54d66d01fbe87cae9ac5195bd52b8bbf7922455
Alex Kiselyov <akiselyov@ukr.net>
2025-08-29 03:01:55 +0400

    LoL Champions: fix GameCalculationConditional

--- bin_defs/defs/GameCalculationConditional.py
+++ bin_defs/defs/GameCalculationConditional.py
@@ -6,8 +6,12 @@ class GameCalculationConditional(BinCalculation):
 
     def calc_string(self, current_block, key):
         default_game_calculation = getf(current_block, 'mDefaultGameCalculation')
-        return self.calc_values(self.all_calculations[default_game_calculation], key)
+        if default_game_calculation and default_game_calculation in self.all_calculations:
+            return self.calc_values(self.all_calculations[default_game_calculation], key)
+        return '0'
     
     def calc_float(self, current_block, key):
         default_game_calculation = getf(current_block, 'mDefaultGameCalculation')
-        return self.calc_values(self.all_calculations[default_game_calculation], key, 'float')
+        if default_game_calculation and default_game_calculation in self.all_calculations:
+            return self.calc_values(self.all_calculations[default_game_calculation], key, 'float')
+        return 0.0
